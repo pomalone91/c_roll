fa4f5a1131bbec8611ab0ac4f29f593f4fbc6855
pomalone91 <pomalone91@gmail.com>
2019-10-19 11:14:20 -0600

    Initial Commit

---
+++ c_roll/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_roll
+//
+//  Created by Paul Malone on 10/19/19.
+//  Copyright © 2019 Paul Malone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
