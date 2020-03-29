3bf315bca58d1d3e16bc71ccc26be2d69550be6a
pomalone91 <pomalone91@gmail.com>
2020-03-29 14:00:49 -0600

    Implements static mode. Refactors some of the flag switching code.

---
+++ Modes/Static.h
@@ -0,0 +1,18 @@
+//
+//  Static.h
+//  c_roll
+//
+//  Created by Paul Malone on 3/29/20.
+//  Copyright © 2020 Paul Malone. All rights reserved.
+//
+
+#ifndef Static_h
+#define Static_h
+
+#include <stdio.h>
+#include "Die.h"
+
+// Public interface
+void staticmode(int argc, const char **arg);
+
+#endif /* Static_h */
--- c_roll/main.c
+++ c_roll/main.c
@@ -18,6 +18,7 @@
 #include "Die.h"
 #include "NCArray.h"
 #include "Interactive.h"
+#include "Static.h"
 
 // Argument flags
 enum flag {bulk, batch, normal};
@@ -25,8 +26,7 @@ enum flag {bulk, batch, normal};
 int main(int argc, const char * argv[]) {
     
     // Function prototypes
-    int staticMode(char *arg);
-    int interactiveMode(void);
+    void staticmode(int argc, const char **arg);
     int bulkMode(char *arg);
     int batchMode(char *arg);
     
@@ -34,7 +34,6 @@ int main(int argc, const char * argv[]) {
     int c;
     char *arg = NULL;
     const char **argvconst = argv;
-    Die d;
     
     /*
      TODO -
@@ -46,55 +45,33 @@ int main(int argc, const char * argv[]) {
      */
     
     printf("argc: %i\n", argc);
-    // Interactive mode
     if (argc == 1) {
         interactive();
-    }
-
-    // Switching on arg flags
-    while ((c = getopt(argc, argvconst, "u:b:")) != -1) {
-        switch (c) {
-            case 'u':
-                printf("bulk\n");
-                arg = optarg;
-                
-                printf("optarg: %s\n", arg);
-                break;
-            case 'b':
-                printf("batch\n");
-                arg = optarg;
-                printf("optarg: %s\n", arg);
-                break;
-            default:
-                printf("normal");
-                arg = optarg;
-                printf("optarg: %s\n", arg);
-                break;
+    } else if (argc > 1 && getopt(argc, argvconst, "u:b:") == -1) {
+        staticmode(argc, argv);
+    } else {
+        // Switching on arg flags
+        while ((c = getopt(argc, argvconst, "u:b:")) != -1) {
+            switch (c) {
+                case 'u':
+                    printf("bulk\n");
+                    arg = optarg;
+                    
+                    printf("optarg: %s\n", arg);
+                    break;
+                case 'b':
+                    printf("batch\n");
+                    arg = optarg;
+                    printf("optarg: %s\n", arg);
+                    break;
+                default:
+                    printf("normal");
+                    arg = optarg;
+                    printf("optarg: %s\n", arg);
+                    break;
+            }
         }
     }
-
-    int i = 1;
-    
-    while (i < argc) {
-        d = init_die((char *)argv[i]);
-        printf("%i ", roll(d));
-        ++i;
-    }
-    printf("\n");
-//    d = init_die((char *)argv[1]);
-    // Uncomment below for debugging
-//    int i = 50; while (i > 0) { printf("%i\n", roll(d)); --i;}
-//    printf("%i\n", roll(d));
-    return 0;
-}
-
-// Functions for each mode
-
-int staticMode(char *arg) {
-    return 0;
-}
-
-int interactiveMode(void) {
     return 0;
 }
 
